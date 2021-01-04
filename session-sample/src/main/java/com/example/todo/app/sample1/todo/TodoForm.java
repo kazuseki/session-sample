cb287512e9f5eaa89a4cdcb707372684f19c4305
kazuseki <47818409+kazuseki@users.noreply.github.com>
2021-01-05 03:00:06 +0900

    Formにバリデーションのグループ化の設定を追加。あとtodoIdも。

--- session-sample/src/main/java/com/example/todo/app/sample1/todo/TodoForm.java
+++ session-sample/src/main/java/com/example/todo/app/sample1/todo/TodoForm.java
@@ -7,18 +7,41 @@ import javax.validation.constraints.Size;
 
 @SuppressWarnings("javadoc")
 public class TodoForm implements Serializable {
-  
+
+  // 入力チェックのグループ化用のインタフェース。
+  // こちらは新規作成処理用。
+  public static interface TodoCreate {
+  };
+
+  // こちらは完了処理用。
+  public static interface TodoFinish {
+  };
+
   private static final long serialVersionUID = 1L;
 
-  @NotNull
+  //アノテーションのgroup属性に、グループを示す任意のjava.lang.Classオブジェクトを設定する
+  @NotNull(groups = { TodoFinish.class })
+  private String todoId;
+
+  // アノテーションのgroup属性に、グループを示す任意のjava.lang.Classオブジェクトを設定する
+  @NotNull(groups = { TodoCreate.class })
   @Size(min = 1, max = 30)
   private String todoTitle;
 
+
+  public String getTodoId() {
+    return todoId;
+  }
+
+  public void setTodoId(String todoId) {
+    this.todoId = todoId;
+  }
+
   public String getTodoTitle() {
-      return todoTitle;
+    return todoTitle;
   }
 
   public void setTodoTitle(String todoTitle) {
-      this.todoTitle = todoTitle;
+    this.todoTitle = todoTitle;
   }
 }
