688fb63f9740e8d1a5ecd217baf63e50e76c8b3c
kazuseki <47818409+kazuseki@users.noreply.github.com>
2021-01-05 04:09:25 +0900

    sample2のURLで使うFormクラスを作成。
    バリデーションを省略するなどシンプルなものにしている。

---
+++ session-sample/src/main/java/com/example/todo/app/sample2/todo/TodoSample2Form.java
@@ -0,0 +1,28 @@
+package com.example.todo.app.sample2.todo;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@SuppressWarnings("javadoc")
+public class TodoSample2Form implements Serializable {
+  
+  private static final long serialVersionUID = 1L;
+
+  // こちらもアノテーションをつけるべきだが、
+  // Controllerの例の中で煩雑になりそうなので、今回は省略する。
+  private String todoId;
+  
+  @NotNull
+  @Size(min = 1, max = 30)
+  private String todoTitle;
+
+  public String getTodoTitle() {
+      return todoTitle;
+  }
+
+  public void setTodoTitle(String todoTitle) {
+      this.todoTitle = todoTitle;
+  }
+}
