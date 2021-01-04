059412fa2d0933df30f4b80002956374cf5ec6da
kazuseki <47818409+kazuseki@users.noreply.github.com>
2021-01-05 01:19:04 +0900

    [add] 空のControllerを追加
    チュートリアルとはパッケージやクラス名を変えている。同じsampleXXXで試験用urlを振り分ける予定のため。

---
+++ session-sample/src/main/java/com/example/todo/app/sample1/todo/TodoSample1Controller.java
@@ -0,0 +1,11 @@
+package com.example.todo.app.sample1.todo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SuppressWarnings("javadoc")
+@Controller
+@RequestMapping("/todo/sample1")
+public class TodoSample1Controller {
+
+}
