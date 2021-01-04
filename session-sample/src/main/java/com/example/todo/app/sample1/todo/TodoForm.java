84d832cd950c25e3cca1509eaddcce1dabade574
kazuseki <47818409+kazuseki@users.noreply.github.com>
2021-01-05 02:30:52 +0900

    Formにバリデーションチェックのアノテーションを追加

--- session-sample/src/main/java/com/example/todo/app/sample1/todo/TodoForm.java
+++ session-sample/src/main/java/com/example/todo/app/sample1/todo/TodoForm.java
@@ -2,11 +2,16 @@ package com.example.todo.app.sample1.todo;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @SuppressWarnings("javadoc")
 public class TodoForm implements Serializable {
   
   private static final long serialVersionUID = 1L;
 
+  @NotNull
+  @Size(min = 1, max = 30)
   private String todoTitle;
 
   public String getTodoTitle() {
