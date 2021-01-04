e1f0a3160c746608b92a7eccae5b78ff58d41f25
kazuseki <47818409+kazuseki@users.noreply.github.com>
2021-01-05 01:42:10 +0900

    一覧表示をするハンドラメソッドをlist1のURLとして追加

--- session-sample/src/main/java/com/example/todo/app/sample1/todo/TodoSample1Controller.java
+++ session-sample/src/main/java/com/example/todo/app/sample1/todo/TodoSample1Controller.java
@@ -1,11 +1,39 @@
 package com.example.todo.app.sample1.todo;
 
+import com.example.todo.domain.model.Todo;
+import com.example.todo.domain.service.todo.TodoService;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+import javax.inject.Inject;
 
 @SuppressWarnings("javadoc")
 @Controller
 @RequestMapping("/todo/sample1")
 public class TodoSample1Controller {
 
+  @Inject
+  TodoService todoService;
+
+  // メソッドに@ModelAttributeアノテーションをつけて、
+  // @RequestMappingが設定されたメソッドの前にこの処理を実行するよう設定する。
+  // また、valueで @SessionAttributes と同じ名前を設定し、sessionスコープで管理するよう設定する。
+  @ModelAttribute
+  public TodoForm setUpForm() {
+      return new TodoForm();
+  }
+
+  @GetMapping("list1")
+  public String list(Model model) {
+      Collection<Todo> todos = todoService.findAll();
+      model.addAttribute("todos", todos);
+      return "todo/list";
+  }
+  
 }
